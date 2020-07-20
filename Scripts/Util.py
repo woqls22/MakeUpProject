3fa3ec1d3764b2dc7e74aebe49b7232ac34b30f3
LEEJAEBEEN <woqls226@gmail.com>
2020-07-20 17:12:48 +0900

    Util Module

--- Scripts/Util.py
+++ Scripts/Util.py
@@ -1,14 +1,14 @@
-import cv2, dlib, sys
+import dlib, sys
+import cv2
 import numpy as np
 from PIL import Image
 MAKE_TRANSPARENT = True
+
 def draw_line(img, L):
   for i in range(len(L)-1):
     x = list(L[i])[0]+3
     y = list(L[i])[1]+3
-    b,g,r = img[y,x]
-    pallete = (int(b),int(g),int(r))
-    #pallete=(255,255,255)
+    pallete=(255,255,255)
     try:
       cv2.line(img,(list(L[i])[0], list(L[i])[1]),(list(L[i+1])[0], list(L[i+1])[1]), color=pallete,thickness=4, lineType=cv2.LINE_AA)
     except:
@@ -63,15 +63,73 @@ def extract_face_part(img,part):
   fromY = 0
   if(int(minY-(maxY-minY)/1.8)>0):
     fromY = int(minY-(maxY-minY)/1.8)
-  return img[fromY:int(maxY+((img.shape[0]-maxY)/2)), minX:maxX]
-def get_rid_of_face_background(imgname):
-  # 회색 계열 : R 146 G 141 B 141
-  # 회색 계열 : R 218 G 216 B 216
-  # 회색 계열 : R 203 G 200 B 200
-  # 회색 계열 : R 233 G 232 B 231
-  # 살색 계열 : R 229 G 190 B 182
-  # 살색 계열 : R 207 G 165 B 159
+  #return img[fromY:maxY, minX:maxX]
+  return img[:, :]
 
+def is_face(item):
+  # Decide whether the face Area is
+  common_mask = 20
+  #Gray Case
+  if(((abs(item[0]-item[1])<common_mask) and (abs(item[1]-item[2])<common_mask) and (abs(item[2]-item[0])<common_mask))):
+    return False
+  #RGB Compare
+  #236 200 188
+  #241 179 172
+  #185 121 114
+  if(item[0]<item[1] or item[0]<item[2]):
+    return False
+  if(item[0]>item[1]+120 or item[0]>item[2]+120):
+    return False
+  if(item[0]<80):
+    return False
+  return True
+def detect_nose_hole(item):
+  #178 121 117
+  #134 76 73
+  #141 86 81
+  #157 102 98
+  #139 81 77
+  if(item[0]<180 and item[1]<130 and item[2]<130):
+    return False
+  return True
+
+def postprocess_face_layer(imgname, nose_x, nose_y):
+  if (MAKE_TRANSPARENT):
+    img = Image.open(imgname)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    opencv_img = cv2.imread(imgname)
+    width = opencv_img.shape[1]
+    height = opencv_img.shape[0]
+    opencv_img = cv2.cvtColor(opencv_img, cv2.COLOR_BGR2RGB)
+
+    newData = []
+    red_mask = 60
+    mask_interval = 150
+    common_mask = 30
+    x = -1
+    y = 0
+    minx = min(nose_x)
+    maxx = max(nose_x)
+    miny = min(nose_y)
+    maxy = max(nose_y)
+    for item in datas:
+      if(x>minx and x<maxx and y>miny and y<maxy):
+        if(detect_nose_hole(item)):
+          newData.append(item)
+        else:
+          newData.append((255, 255, 255, 0))
+      else:
+        newData.append(item)
+      x = x + 1
+      if (x >= width):
+        y = y + 1
+        x = 0
+    img.putdata(newData)
+    img.save(imgname, "PNG")
+    print(imgname)
+
+def get_rid_of_face_background(imgname, minX,maxX,maxY):
   if(MAKE_TRANSPARENT):
     img = Image.open(imgname)
     img = img.convert("RGBA")
@@ -80,25 +138,42 @@ def get_rid_of_face_background(imgname):
     width = opencv_img.shape[1]
     height = opencv_img.shape[0]
     opencv_img = cv2.cvtColor(opencv_img, cv2.COLOR_BGR2RGB)
-    color = [230,190,180]
+    color = [140,230,200]
 
     newData = []
-    red_mask = 150
+    red_mask = 60
     mask_interval = 150
-    common_mask = 20
-
+    common_mask = 30
+    x=-1
+    y=0
     for item in datas:
       #print("{0}, {1}, {2}".format(item[0],item[1],item[2]))
       #print("{0}, {1}, {2}".format(color[0],color[1],color[2]))
       #print("========================")
-      if(item[0]<177 or item[2]>230):# Gray Filtering
+      #print(item)
+      x=x+1
+      if(y>maxY):
+        if(is_face(item) or (x>minX and x<maxX)):
+          #print(x)
+          if(is_face(item)):
+            newData.append(item)
+          else:
+            newData.append((255, 255, 255, 0))
+            pass
+        else:
+          newData.append((255, 255, 255, 0))
+      elif(item[0]<140 or item[2]>200 or ((abs(item[0]-item[1])<common_mask) and (abs(item[1]-item[2])<common_mask) and (abs(item[2]-item[0])<common_mask))):# Gray Filtering
         newData.append((255, 255, 255, 0))
-      elif(item[0]==item[1] and item[1]==item[2]):
-        newData.append((255, 255, 255, 0))
-      elif ((item[0] <= color[0]+red_mask and item[0]>color[0]-red_mask) and (item[1] <= color[1]+mask_interval and item[1]>color[1]-mask_interval) and (item[2] <= color[2]+mask_interval and item[2]>color[2]-mask_interval)):
-        newData.append(item)
-        print(item)
-      elif((abs(item[0]-item[1])<common_mask) and (abs(item[1]-item[2])<common_mask) and (abs(item[2]-item[0])<common_mask)):
+      elif ((item[0] <= color[0]+red_mask and item[0]>color[0]-red_mask) or (item[1] <= color[1]+mask_interval or item[1]>color[1]-mask_interval) or (item[2] <= color[2]+mask_interval and item[2]>color[2]-mask_interval)):
+        if ((item[0] > item[1] + 85) and (item[0] > item[2] + 85)):
+          newData.append((255, 255, 255, 0))
+        elif(item[0]>140 and item[1]<140 and item[2]<140):
+          newData.append((255, 255, 255, 0))
+        elif(item[0]>225 and item[1]<150 and item[2]<150):
+          newData.append((255, 255, 255, 0))
+        else:
+           newData.append(item)
+      elif((item[0]>item[1]+50) or (item[0]>item[2]+50)):
         newData.append((255, 255, 255, 0))
       elif(item[0]<item[2] and item[0]<item[1]):
         newData.append((255, 255, 255, 0))
@@ -106,12 +181,15 @@ def get_rid_of_face_background(imgname):
         newData.append((255, 255, 255, 0))
       else:
         newData.append((255, 255, 255, 0))
+      if (x >= width):
+        y = y + 1
+        x=0
     img.putdata(newData)
     img.save(imgname, "PNG")
-    opencv_img = cv2.imread(imgname)
-    cv2.GaussianBlur(opencv_img, (3,3),0)
-    cv2.imwrite(imgname, opencv_img)
-    print("Transparent Task Done.")
+    print(imgname)
+    #opencv_img = cv2.imread(imgname)
+    #cv2.GaussianBlur(opencv_img, (3,3),0)
+    #cv2.imwrite(imgname, opencv_img)
 
     '''
      print(mouse)
@@ -138,64 +216,25 @@ def is_in_Area(x,y,minX,maxX,minY,maxY):
     return True
   else:
     return False
-def get_face_layer(imgname, mouse, left_eyes,right_eyes,nose,face_line,left_eyes_brow,right_eyes_brow):
-  if(MAKE_TRANSPARENT):
-    img = cv2.imread(imgname)
-    mouse = mouse-mouse.min(axis=0)
-    left_eyes = left_eyes - left_eyes.min(axis=0)
-    right_eyes = right_eyes - right_eyes.min(axis=0)
-    nose = nose - nose.min(axis=0)
-    face_line = face_line - face_line.min(axis=0)
-    left_eyes_brow = left_eyes_brow - left_eyes_brow.min(axis=0)
-    right_eyes_brow = right_eyes_brow - right_eyes_brow.min(axis=0)
 
-    rect_mouse = cv2.boundingRect(mouse)
-    x, y, w, h = rect_mouse
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [mouse], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
+def erase_layer(img,version, left_eyes_brow, right_eyes_brow, left_eyes, right_eyes, mouse):
+  # PreProcessing
+  if (MAKE_TRANSPARENT):
+    cvImg = img
+    #points1 = np.array(left_eyes_brow, np.int32)
+    points2 = np.array(right_eyes_brow, np.int32)
+    points3 = np.array(left_eyes, np.int32)
+    points4 = np.array(right_eyes, np.int32)
+    points5 = np.array(mouse,np.int32)
+    #cvImg = cv2.fillConvexPoly(cvImg, points1, (0, 0, 0))
+    #cvImg = cv2.fillConvexPoly(cvImg, points2, (0, 0, 0))
+    cvImg = cv2.fillConvexPoly(cvImg, points3, (0, 0, 0))
+    cvImg = cv2.fillConvexPoly(cvImg, points4, (0, 0, 0))
+    #cvImg = cv2.fillConvexPoly(cvImg, points5, (0,0,0,))
+    cv2.imwrite('./FacePart/face_line_img'+version+'.png', cvImg)
+    #cv2.imshow('test', cvImg)
 
-    rect_left_eyes = cv2.boundingRect(left_eyes)
-    x, y, w, h = rect_left_eyes
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [left_eyes], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-    rect_right_eyes = cv2.boundingRect(right_eyes)
-    x, y, w, h = rect_right_eyes
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [right_eyes], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-    rect_nose = cv2.boundingRect(nose)
-    x, y, w, h = rect_nose
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [nose], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-    rect_face_line = cv2.boundingRect(face_line)
-    x, y, w, h = rect_face_line
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [face_line], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-    rect_left_eyes_brow = cv2.boundingRect(left_eyes_brow)
-    x, y, w, h = rect_left_eyes_brow
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [left_eyes_brow], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
-    rect_right_eyes_brow = cv2.boundingRect(right_eyes_brow)
-    x, y, w, h = rect_right_eyes_brow
-    croped = img[y:y + h, x:x + w].copy()
-    mask = np.zeros(croped.shape[:2], np.uint8)
-    cv2.drawContours(mask, [right_eyes_brow], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    dst = cv2.bitwise_and(croped, croped, mask=mask)
 
-    bg = np.ones_like(croped, np.uint8)*255
-    cv2.bitwise_not(bg,bg, mask=mask)
-    dst2 = bg+ dst
 
 def get_rid_of_background(imgname):
   # 회색 계열 : R 146 G 141 B 141
@@ -221,7 +260,7 @@ def get_rid_of_background(imgname):
       #print("========================")
       if ((item[0] <= color[0]+red_mask and item[0]>color[0]-red_mask) and (item[1] <= color[1]+mask_interval and item[1]>color[1]-mask_interval) and (item[2] <= color[2]+mask_interval and item[2]>color[2]-mask_interval)):
         newData.append((255, 255, 255, 0))
-      elif(abs(item[0]-item[1])<10 and abs(item[1]-item[2])<10 and abs(item[2]-item[0])<10):
+      elif(abs(item[0]-item[1])<20 and abs(item[1]-item[2])<20 and abs(item[2]-item[0])<20):
         newData.append((255, 255, 255, 0))
       else:
         newData.append(item)
@@ -232,4 +271,3 @@ def get_rid_of_background(imgname):
     cv2.GaussianBlur(opencv_img, (3,3),0)
     cv2.imwrite(imgname, opencv_img)
 
-    print("Transparent Task Done.")
